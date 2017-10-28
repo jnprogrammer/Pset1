56fa845eaa4d10950074c6c02bc3065b06852820
Joshua Parker <joshuanparker@gmail.com>
2017-10-28 17:04:52 +0000

    Adding Super Mario, version2

---
+++ super_mario.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // widenColumn when != 0 will add additional hashes to the base colum of the pyramid
+    int height = 0, widenColumn = 0, tempHt = 0;
+    do
+    {
+        printf("Please enter a non negative number smaller than 23 \n");
+        height = get_int();
+    }
+    while(height >23 || height < 0);
+    //Set tempHt to height
+    tempHt = height;
+
+    for(int j = 0; j < height; j++)
+    {
+       // installs spaces n times, n = height minus one.
+       for(int i = height - j; i > 1; i--)
+       {
+           printf(" ");
+       }
+       //installs #
+       for(int x = 0 ;x <= j + widenColumn; x++)
+       {
+           printf("#");
+       }
+       //prints the double spaces
+       printf("  ");
+
+       tempHt--;
+       for(int i = tempHt - widenColumn; i < height; i++)
+       {
+          printf("#");
+       }
+       printf("\n");
+    }
+}
